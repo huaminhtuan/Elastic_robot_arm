d4184b4bcf4b83da6d00d2e58f0af35923ca7f70
Tuan Hua <huaminhtuan94@gmail.com>
2020-07-05 01:38:59 +0700

    fix legend missing when exporting figure to image

--- matlab/simulation/convert_fig_to_image.m
+++ matlab/simulation/convert_fig_to_image.m
@@ -4,11 +4,14 @@ path = 'decentralised\';
 d=dir(strcat(path,'*.fig')); % capture everything in the directory with FIG extension
 allNames={d.name}; % extract names of all FIG-files
 close all; % close any open figures
-for i=1:1%length(allNames)
-	open(strcat(path,allNames{i})); % open the FIG-file
+for i=1:length(allNames)
+	file = open(strcat(path,allNames{i})); % open the FIG-file
 	base=strtok(allNames{i},'.'); % chop off the extension (".fig")
-    legend = findobj(gcf,'Type','legend');
-    set(legend,'FontName', 'Helvetica');
+    handles = findobj(gcf,'Type','axes');
+    for j=1:length(handles)
+        legend(handles(j), 'off')
+        legend(handles(j), 'show')
+    end
 	print('-djpeg',base); % export to JPEG as usual
 	close(gcf); % close it ("gcf" returns the handle to the current figure)
 end
